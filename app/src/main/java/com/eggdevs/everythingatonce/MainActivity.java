3285a96b0e60e296b699ff62fd8383ac9d35af38
AyushMax21 <yt.ayush02@gmail.com>
2021-07-15 19:09:12 +0530

    OnClickListener Implemented

--- app/src/main/java/com/eggdevs/everythingatonce/MainActivity.java
+++ app/src/main/java/com/eggdevs/everythingatonce/MainActivity.java
@@ -1,16 +1,38 @@
 package com.eggdevs.everythingatonce;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+   Button btnOpenUrl, btnLocation, btnSendMessage, btnCallSomeone;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
-
       getWindow().setFlags(1024, 1024);
+
+      btnCallSomeone = findViewById(R.id.btnCallSomeone);
+      btnLocation = findViewById(R.id.btnLocation);
+      btnSendMessage = findViewById(R.id.btnSendMessage);
+      btnOpenUrl = findViewById(R.id.btnOpenUrl);
+
+      btnCallSomeone.setOnClickListener(this);
+      btnLocation.setOnClickListener(this);
+      btnSendMessage.setOnClickListener(this);
+      btnOpenUrl.setOnClickListener(this);
+
    }
+
+   @Override
+   public void onClick(View view) {
+
+   }
+
+   //alt + shitft + left mouse click
+   //ctrl + d
 }
