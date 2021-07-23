dadb81f3e34cb479a647a605b878061411d771b3
AyushMax21 <yt.ayush02@gmail.com>
2021-07-23 19:24:07 +0530

    Added Map activity and Browser activity

--- app/src/main/java/com/eggdevs/everythingatonce/LocationActivity.java
+++ app/src/main/java/com/eggdevs/everythingatonce/LocationActivity.java
@@ -2,13 +2,35 @@ package com.eggdevs.everythingatonce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class LocationActivity extends AppCompatActivity {
+
+   EditText etLocation;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_location);
+
+      etLocation = findViewById(R.id.etLocation);
+
+      findViewById(R.id.btnMap).setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+            String location = etLocation.getText().toString();
+
+            Uri mapUri = Uri.parse("geo:0,0?q=" + location);
+
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapUri);
+
+            Intent mapIntentChooser = Intent.createChooser(mapIntent, "Map Intent Chooser");
+            startActivity(mapIntentChooser);
+         }
+      });
    }
 }
--- app/src/main/java/com/eggdevs/everythingatonce/UrlActivity.java
+++ app/src/main/java/com/eggdevs/everythingatonce/UrlActivity.java
@@ -2,13 +2,42 @@ package com.eggdevs.everythingatonce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class UrlActivity extends AppCompatActivity {
+
+   Button btnUrl;
+   EditText etUrl;
+
+   //web: https:// + website url
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_url);
+
+      btnUrl  = findViewById(R.id.btnUrl);
+      etUrl = findViewById(R.id.etUrl);
+
+      btnUrl.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+
+            String url = etUrl.getText().toString();
+
+            Uri webAddressUri = Uri.parse("https://" + url);
+
+            Intent urlIntent = new Intent(Intent.ACTION_VIEW, webAddressUri);
+            Intent urlIntentChooser = Intent.createChooser(urlIntent, "Url Intent Chooser");
+
+            startActivity(urlIntentChooser);
+         }
+      });
+
    }
 }
